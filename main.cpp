f4941e6f190dbb44851186d6d8e73b54756850bc
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2023-03-30 19:08:01 +0300

    mov instruction parsing

--- main.cpp
+++ main.cpp
@@ -3,5 +3,22 @@
 
 i32 main(i32, char const**) {
     initCore();
+    u8 input[] = {0x89, 0xD9}; // mov cx, bx
+
+    u8 opcode;
+    u8 d;
+    u8 w;
+    u8 mod;
+    u8 reg;
+    u8 rm;
+
+    opcode = input[0] >> 2;
+    d = (input[0] >> 1) & 1;
+    w = input[0] & 1;
+
+    mod = input[1] >> 6;
+    reg = (input[1] >> 3) & 3;
+    rm = input[1] & 3;
+
     return 0;
 }

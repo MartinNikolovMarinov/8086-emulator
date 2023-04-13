9741d44e480227d63d86bc9ff92b9c2016571e13
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2023-04-13 12:00:31 +0300

    mov instruction parsing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <init_core.h>
 #include <fmt/format.h>
 
+struct Instruction {
+    u8 byte1;
+    u8 byte2;
+
+    u8 opcode() const { return byte1 >> 2; }
+    u8 d() const { return (byte1 >> 1) & 1; }
+    u8 w() const { return byte1 & 1; }
+    u8 mod() const { return byte2 >> 6; }
+    u8 reg() const { return (byte2 >> 3) & 3; }
+    u8 rm() const { return byte2 & 3; }
+};
+
 i32 main(i32, char const**) {
     initCore();
-    u8 input[] = {0x89, 0xD9}; // mov cx, bx
+    u8 input[] = {0xD9, 0x89}; // mov cx, bx
 
-    u8 opcode;
-    u8 d;
-    u8 w;
-    u8 mod;
-    u8 reg;
-    u8 rm;
+    Instruction instruction;
+    instruction.byte1 = input[0];
+    instruction.byte2 = input[1];
 
-    opcode = input[0] >> 2;
-    d = (input[0] >> 1) & 1;
-    w = input[0] & 1;
-
-    mod = input[1] >> 6;
-    reg = (input[1] >> 3) & 3;
-    rm = input[1] & 3;
+    fmt::print("opcode: {}\n", instruction.opcode());
+    fmt::print("d: {}\n", instruction.d());
+    fmt::print("w: {}\n", instruction.w());
+    fmt::print("mod: {}\n", instruction.mod());
+    fmt::print("reg: {}\n", instruction.reg());
+    fmt::print("rm: {}\n", instruction.rm());
 
     return 0;
 }

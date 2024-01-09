f9f29190de0702fdf8bb222c833d9b33f0a4cb94
Martin Marinov <martin_n_marinov@protonmail.com>
2024-01-09 22:43:35 +0200

    Uploading binaries for all asm files.

--- scripts/compile_asm_into_build_dir.sh
+++ scripts/compile_asm_into_build_dir.sh
@@ -8,6 +8,7 @@ pushd data > /dev/null
 # compile all asm files into object files:
 for f in ./*.asm; do
     nasm "$f" -o ../build/"${f%}.o"
+    nasm "$f" -o "${f%}.o" # Create in the same directory as well
 done
 
 pushd my_examples > /dev/null
@@ -15,6 +16,7 @@ pushd my_examples > /dev/null
 # compile all asm files into object files:
 for f in ./*.asm; do
     nasm "$f" -o ../../build/my_examples/"${f%}.o"
+    nasm "$f" -o "${f%}.o" # Create in the same directory as well
 done
 
 popd > /dev/null
